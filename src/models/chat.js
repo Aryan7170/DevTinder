d4434a6131f7e1800137c203d5ab6b87b890a267
aryanduhoon7170@gmail.com <aryanduhoon7170@gmail.com>
2026-05-26 18:43:02 +0530

    uri fix

--- src/models/chat.js
+++ src/models/chat.js
@@ -1,45 +1,27 @@
 const mongoose = require("mongoose");
 
-const connectionRequestSchema = new mongoose.Schema(
+const messageSchema = new mongoose.Schema(
   {
-    fromUserId: {
+    senderId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
     },
-    toUserId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    status: {
+    text: {
       type: String,
       required: true,
-      enum: {
-        values: ["ignored", "interested", "accepted", "rejected"],
-        message: `{VALUE} is incorrect status type`,
-      },
     },
   },
   { timestamps: true },
 );
 
-// ConnectionRequest.find({fromUserId: 273478465864786587, toUserId: 273478465864786587})
-
-connectionRequestSchema.index({ fromUserId: 1, toUserId: 1 });
-
-connectionRequestSchema.pre("save", function (next) {
-  const connectionRequest = this;
-  // Check if the fromUserId is same as toUserId
-  if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
-    throw new Error("Cannot send connection request to yourself!");
-  }
-  next();
+const chatSchema = new mongoose.Schema({
+  participants: [
+    { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  ],
+  messages: [messageSchema],
 });
 
-const ConnectionRequestModel = new mongoose.model(
-  "ConnectionRequest",
-  connectionRequestSchema,
-);
+const Chat = mongoose.model("Chat", chatSchema);
 
-module.exports = ConnectionRequestModel;
+module.exports = { Chat };
